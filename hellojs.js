d5d01d64206bf5b8fa619540f78a1d70d7787730
sina <sinasthlm@yahoo.com>
2018-06-10 11:12:08 +0200

    initial commit

---
+++ hellojs.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', getWeather);
+
+document.getElementById('someRandomID').addEventListener('mousedown', abotherJSFunc);
+
+
+function getWeather() {
+  console.log('this is a test msg');
+  console.log('this is a test msg');
+  console.log('this is a test msg');
+}
+
+function abotherJSFunc() {
+
+  switch (key) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
+  for (let idx = 0; idx < array.length; idx++) {
+    if (true) {
+      if (condition) {
+        
+      }
+    } else {
+      
+    }
+    const elem = array[idx];
+    console.log("current element index is " + elem);
+  }
+  console.log("exceeded my expectations");
+  
+}
